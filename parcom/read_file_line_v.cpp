99fe1a775d60bb11f802c109b0f434ff5fd68008
Vivek Pandya <vivekvpandya@gmail.com>
2016-03-13 02:24:49 +0530

    Editor's code in place and removed unused comments

--- parcom/read_file_line_v.cpp
+++ parcom/read_file_line_v.cpp
@@ -123,7 +123,7 @@ void dumpRecvNews( int rank, News *news, int size) {
                 printf("News Details: %s \n", news[i].details);
             }
             printf("Process %d dump ends ------------------------------ \n\n",p);
-            //MPI_Barrier(MPI_COMM_WORLD);
+           
             usleep(10000);
         }
     }
@@ -174,7 +174,6 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int repo_count = world_size - 1;
     
-    //printf("Rank %d says World Size is %dn", world_rank,world_size);
     
     process_rank =     (int*)malloc(sizeof(int) * (world_size - 1));
     
@@ -182,38 +181,7 @@ int main(int argc, char **argv) {
         process_rank[i] = i+1;
     }
     
-    
-    
-    
-    
-    //printf("Time Stamp : %sn",news1.timeStamp);
-    //printf("Title : %sn",news1.title);
-    //printf("Details : %sn",news1.details);
-    /*
-    for (int i = 1 ; i <= count ; i++) {
-        printf("News item : %d n", i);
-        printf("News TimeStamp: %s n", news[i].timeStamp);
-        printf("News Title: %s n", news[i].title);
-        printf("News Details: %s n", news[i].details);
-    }
-    */
-    
-    
-    /*
-    News *latestNews = findLatest(news, count);
-    printf("Latest News TimeStamp: %s n", latestNews->timeStamp);
-    printf("Latest News Title: %s n", latestNews->title);
-    printf("Latest News Details: %s n", latestNews->details);
-    */
-    //%a %b %e %T %Z %Y => Thu Mar  3 22:32:41 IST 2016
-    //struct tm time;
-    //strptime(news1.timeStamp,"%a %b %e %T %Z %Y",&time);
-    //time_t loctime = mktime(&time);
-    //printf ( "Current local time and date: %s", asctime (&time) );
-    
-    
-    
-    
+        
     //number of items inside structure Test
     const int nitems = 3;
     
@@ -254,29 +222,18 @@ int main(int argc, char **argv) {
     //commit the new data type
     MPI_Type_commit(&mpi_test_type);
     
-    //get rank of current process
-    //MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     
     //Code for the creation of REPORTER COMM goes here.
-    
-    
-    
+       
     MPI_Comm_dup(MPI_COMM_WORLD, &world_comm);
     MPI_Comm_group(world_comm, &world_group);
     MPI_Group_incl(world_group, (world_size - 1), process_rank, &new_group);
     MPI_Comm_create(world_comm, new_group, &rep_comm);
     
-    
-    //if(rep_comm == MPI_COMM_NULL){
-        //    printf("%d is not included in the new reporters comm worldn",world_rank);
-    //}
-    //printf("%dn",error);
-    
-    //Get the size of the Comm REPORTER.
-    
+       
     if(world_rank == 0){
         //Do editor's task
-        //printf("%sn","Waiting for the reporters to send a valid and latest newsn" );
+       
         MPI_Status status;
         News recv_news;
         int number_amount;
@@ -290,8 +247,7 @@ int main(int argc, char **argv) {
         while(TRUE){
             
             MPI_Recv(&recv_news,1,mpi_test_type,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-            //cout << "News received with tag " << status.MPI_TAG << endl;
-            
+           
             if(status.MPI_TAG == 1){
                 reps_done++;
                 
@@ -299,7 +255,7 @@ int main(int argc, char **argv) {
                     break;
                 }
                 }else{
-                //print_news(recv_news);
+              
                 insert_news(news_db,recv_news);
             }
             
@@ -308,7 +264,7 @@ int main(int argc, char **argv) {
         }
         
         
-        cout << "Received all the messages..\nPrinting the news items\n";
+        cout << "\n\n\nReceived all the messages..\nPrinting the news items\n";
         
        
         print_news_items(news_db);
@@ -329,8 +285,7 @@ int main(int argc, char **argv) {
         News *news;
         News Test;
         int count = atoi(argv[1]);
-        //printf("Count %d",count);
-        //news = (News *)malloc(sizeof(News)*(count));
+        
         int loadBalance = count % repo_count;
         int actual_count  =  count;
         if (  loadBalance != 0) {
@@ -350,21 +305,17 @@ int main(int argc, char **argv) {
                 char *arg = (char *)malloc(sizeof(char)*32);
                 snprintf(arg, 32, "%d", i);
                 strcat(filePath,arg);
-                //printf("So file path is : %sn",filePath);
+               
                 fp = fopen(filePath, "r");
                 if (fp == NULL) {
                     exit(EXIT_FAILURE);
                 }
-                //news[i] = (News *)malloc(sizeof(News));
+                
                 int j = 0 ;
                 while ((read = getline(&line, &len, fp)) != -1) {
-                    //printf("Retrieved line of length %zu :n", read);
-                    //printf("%s", line);
                     
                     if (j == 0) {
-                        //news[i]->timeStamp = (char *)malloc(sizeof(char)*read);
-                        //strncpy(news[i].timeStamp,line,read);
-                        //news[i].timeStamp[read] = '';
+                       
                         strncpy(&(buffer[p]),line,read);
                         if (read < 30) {
                             int k = p + read;
@@ -377,9 +328,7 @@ int main(int argc, char **argv) {
                         j++;
                         p = p+ 30;
                         } else if ( j ==  1) {
-                        //news[i]->title = (char *)malloc(sizeof(char)*read);
-                        //strncpy(news[i].title,line,read);
-                        //news[i].title[read] = '';
+                        
                         strncpy(&(buffer[p]),line,read);
                         if (read < 100) {
                             int k = p + read;
@@ -392,9 +341,7 @@ int main(int argc, char **argv) {
                         j++;
                         p = p+ 100;
                         } else {
-                        //news[i]->details = (char *)malloc(sizeof(char)*read);
-                        //strncpy(news[i].details,line,read);
-                        //news[i].details[read] = '';
+                        
                         strncpy(&(buffer[p]),line,read);
                         if (read < 500) {
                             int k = p + read;
@@ -429,113 +376,26 @@ int main(int argc, char **argv) {
                 printf("%c", buffer[i]);
             }
         }
-        /*
-        for (int i = 1 ; i <= count ; i++) {
-            printf("News item : %d n", i);
-            printf("News TimeStamp: %s n", news[i].timeStamp);
-            printf("News Title: %s n", news[i].title);
-            printf("News Details: %s n", news[i].details);
-        }
-        MPI_Barrier(MPI_COMM_WORLD);
-        /*
-        if(rank == 1) {
-            // News send;
-            // News.one = 1;
-            // News.two = 2.0;
-            // strncpy(send.news,"This is simple news.",sizeof(send.news));
-            const int dest = 2;
-            MPI_Send(news[1], 1, mpi_test_type, dest, tag, MPI_COMM_WORLD);
-            printf("nRank %d sending n %s  n %s n %sn", rank, news[1]->timeStamp, news[1]->title, news[1]->details);
-        }
-        if(rank == 2) {
-            MPI_Status status;
-            const int src = 1;
-            News recv;
-            MPI_Recv(&recv, 1, mpi_test_type, src, tag, MPI_COMM_WORLD, &status);
-            printf("nRank %d received n %s n %s n %s n", rank, recv.timeStamp,recv.title,recv.details);
-        }
         
-        */
-        //News *recvNews = (News *)malloc(sizeof(News )*(count));
         char *recvBuffer = (char *)malloc(sizeof(char) * (count*630));
         int proc_count = count / repo_count;
         int proc_size = 630 * ( proc_count ) ; // this should always be divisible
-        /*
-        if ( rep_rank == 1) {
-            printf("Repo count : %d n", repo_count);
-            printf("Count : %d n", count);
-            printf("Proc_size : %d n", proc_size );
-        }
-        */
+       
         int status = MPI_Alltoall(buffer,proc_size,MPI_CHAR,recvBuffer,proc_size,MPI_CHAR,rep_comm);
         
         if(status != 0) {
             printf("MPI_Alltoall failed with status %d\n", status);
             exit(EXIT_FAILURE);
         }
-        //MPI_Barrier(rep_comm);
+       
         printf(" \n \n \n");
-        /*
-        if (rank == 1) {
-            int limit = count * 630 -1;
-            for (int i = 0 ; i < limit; i++) {
-                printf("%c", recvBuffer[i]);
-            }
-            
-        }
-        */
-        //MPI_Barrier(MPI_COMM_WORLD);
+       
         News** newsArray = getNewsArray(recvBuffer,count,repo_count,proc_count,9999);
         
         
-        //if (rep_rank == 0) {
-            /*
-            for( int n_items = 0; n_items < proc_count ;  n_items++) {
-                printf("News item : %d n",n_items);
-                for ( int r_count = 0; r_count < repo_count; r_count++) {
-                    printf("Time stamp : %s n", newsArray[n_items][r_count].timeStamp);
-                    printf("Title : %s n", newsArray[n_items][r_count].title);
-                    printf("Details : %s n", newsArray[n_items][r_count].details);
-                }
-            } */
-            /*
-            int in = 0;
-            for( int n_items = 0; n_items < proc_count ;  n_items++) {
-                
-                for ( int r_count = 0; r_count < repo_count; r_count++) {
-                    printf("n");
-                    for (int i = 0; i < 29; i++) {
-                        printf("%c",recvBuffer[in+i]);
-                    }
-                    in = in + 29;
-                    for (int i = 0; i < 99; i++) {
-                        printf("%c",recvBuffer[in+i]);
-                    }
-                    in = in + 99;
-                    for (int i = 0; i < 499; i++) {
-                        printf("%c",recvBuffer[in+i]);
-                    }
-                    in = in + 499;
-                }
-            } */
-            /*
-            for( int n_items = 0; n_items < proc_count ;  n_items++) {
-                
-                for ( int r_count = 0; r_count < repo_count; r_count++) {
-                    printf("n");
-                    printf("Timestamp : %s n", newsArray[n_items][r_count].timeStamp);
-                    printf("Title : %s n", newsArray[n_items][r_count].title);
-                    printf("Details : %s n", newsArray[n_items][r_count].details);
-                }
-            }
-            */
-        //}
-
-        
-        
-        
+       
         for( int my_rank = 0; my_rank < repo_count; my_rank++) {
-            //printf("Proc_count : %d n",proc_count);
+            
             if( my_rank == rep_rank ) {
                 int news_item = 0;
                 for ( int news_item = 0 ; news_item < proc_count ; news_item++ ) {
@@ -552,34 +412,16 @@ int main(int argc, char **argv) {
         		//Tag 1 is used when there is no news to be sent to the editor
         		// and hence signal that the work has been done.
         		News dummynews;
-        		//cout << "Sending the done flag from node " << rep_rank << endl;
-       			 MPI_Send(&dummynews,1,mpi_test_type,0,1,MPI_COMM_WORLD);
+        		
+       			MPI_Send(&dummynews,1,mpi_test_type,0,1,MPI_COMM_WORLD);
         
                 
             }
         }
         
       
-        /*
-        else if(rank == 1) {
-            for (int i = 1 ; i <= size ; i++) {
-                printf("News item : %d rank %dn", i , rank);
-                printf("News TimeStamp: %s rank %dn", recvNews[i].timeStamp,rank);
-                printf("News Title: %s rank %dn", recvNews[i].title,rank);
-                printf("News Details: %s rank %d n", recvNews[i].details,rank);
-            }
-        }
-        */
-        //dumpRecvNews(,recvNews,size);
-        
-        
-        //free the derived data type
         
     }
-    
-    
-    
-    
     
     MPI_Type_free(&mpi_test_type);
     MPI_Finalize();
